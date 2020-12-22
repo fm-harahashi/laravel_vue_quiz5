26e1a9b6c681ec62c5de6d3884e0d5b6bc95ec3d
harahashinaoya <mogamimaly3603@gmail.com>
2020-12-22 11:24:59 +0900

    11-2 Laravel-Adminのカスタマイズ -お知らせ・カテゴリー・一般ユーザー管理の実装- ルーティングを編集(categoriesを追加)

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -12,4 +12,5 @@ Route::group([
 
     $router->get('/', 'HomeController@index')->name('admin.home');
     $router->resource('/information', InformationController::class);
+    $router->resource('/categories', CategoryController::class);
 });
